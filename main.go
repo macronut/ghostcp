2c980b833eefab816fc8fa0efe9e80d639d281c6
macronut <macronut@live.com>
2019-11-04 15:18:13 +0800

    Other platforms will create a new project

--- main.go
+++ main.go
@@ -1,5 +1,147 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"./header"
+	"github.com/chai2010/winsvc"
+)
+
+var ServiceMode bool = true
+
+func StartService() {
+	runtime.GOMAXPROCS(1)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	if tcpioneer.LogLevel > 0 {
+		var logFilename string = "tcpioneer.log"
+		logFile, err := os.OpenFile(logFilename, os.O_RDWR|os.O_CREATE, 0777)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer logFile.Close()
+
+		tcpioneer.Logger = log.New(logFile, "\r\n", log.Ldate|log.Ltime|log.Lshortfile)
+	}
+
+	err := tcpioneer.LoadConfig()
+	if err != nil {
+		if tcpioneer.LogLevel > 0 || !ServiceMode {
+			log.Println(err)
+		}
+		return
+	}
+
+	if tcpioneer.LogLevel == 0 && !ServiceMode {
+		tcpioneer.LogLevel = 1
+	}
+
+	tcpioneer.TCPDaemon(":443", false)
+	tcpioneer.TCPDaemon(":80", false)
+	tcpioneer.UDPDaemon(443, false)
+
+	if tcpioneer.Forward {
+		tcpioneer.TCPDaemon(":443", true)
+		tcpioneer.TCPDaemon(":80", true)
+		tcpioneer.UDPDaemon(443, true)
+	}
+
+	if tcpioneer.DNS == "" {
+		tcpioneer.DNSRecvDaemon()
+	} else {
+		tcpioneer.TCPDaemon(tcpioneer.DNS, false)
+		tcpioneer.DNSDaemon()
+	}
+
+	fmt.Println("Service Start")
+	tcpioneer.Wait()
+}
+
+func StopService() {
+	arg := []string{"/flushdns"}
+	cmd := exec.Command("ipconfig", arg...)
+	d, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Println(string(d), err)
+	}
+
+	os.Exit(0)
+}
+
 func main() {
-	start()
+	serviceName := "TCPPioneer"
+	var flagServiceInstall bool
+	var flagServiceUninstall bool
+	var flagServiceStart bool
+	var flagServiceStop bool
+	flag.BoolVar(&flagServiceInstall, "install", false, "Install service")
+	flag.BoolVar(&flagServiceUninstall, "remove", false, "Remove service")
+	flag.BoolVar(&flagServiceStart, "start", false, "Start service")
+	flag.BoolVar(&flagServiceStop, "stop", false, "Stop service")
+	flag.Parse()
+
+	appPath, err := winsvc.GetAppPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// install service
+	if flagServiceInstall {
+		if err := winsvc.InstallService(appPath, serviceName, ""); err != nil {
+			log.Fatalf("installService(%s, %s): %v\n", serviceName, "", err)
+		}
+		log.Printf("Done\n")
+		return
+	}
+
+	// remove service
+	if flagServiceUninstall {
+		if err := winsvc.RemoveService(serviceName); err != nil {
+			log.Fatalln("removeService:", err)
+		}
+		log.Printf("Done\n")
+		return
+	}
+
+	// start service
+	if flagServiceStart {
+		if err := winsvc.StartService(serviceName); err != nil {
+			log.Fatalln("startService:", err)
+		}
+		log.Printf("Done\n")
+		return
+	}
+
+	// stop service
+	if flagServiceStop {
+		if err := winsvc.StopService(serviceName); err != nil {
+			log.Fatalln("stopService:", err)
+		}
+		log.Printf("Done\n")
+		return
+	}
+
+	// run as service
+	if !winsvc.IsAnInteractiveSession() {
+		log.Println("main:", "runService")
+
+		if err := os.Chdir(filepath.Dir(appPath)); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := winsvc.RunAsService(serviceName, StartService, StopService, false); err != nil {
+			log.Fatalf("svc.Run: %v\n", err)
+		}
+		return
+	}
+
+	ServiceMode = false
+	StartService()
 }
